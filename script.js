82cbd30e0f404a7e2022a676b68987fed0194724
lowchain3071 <136857408+lowchain3071@users.noreply.github.com>
2023-06-22 01:07:48 +0000

    edited script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 640;
+const CANVAS_HEIGHT = canvas.height = 360;
+console.log(ctx);
 
+//player stats
+let health = 100;
+let money = 20;
+let energy = 100;
+
+//messages
+let bottomBarMessage = "Navigate Using Arrow Keys";
+let middleMessage = "HAHA FUNNY RPG"
+let optionA = "Load Game";
+let optionB = "New Game"
+let optionC = "credits";
+let optionD = null;
+
+//option selection
+
+window.addEventListener('keydown', e =>{
+  
+});
+
+function animate(){
+
+  
+  requestAnimationFrame(animate);
+}
+animate();
