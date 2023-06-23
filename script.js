aa9e0d6ee241011917571be0f124f870476b7d8b
lowchain3071 <136857408+lowchain3071@users.noreply.github.com>
2023-06-23 01:38:19 +0000

    edited script.js

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ let bottomBarMessage = "Navigate Using Arrow Keys";
 let middleMessage = "HAHA FUNNY RPG"
 let optionA = "Load Game";
 let optionB = "New Game"
-let optionC = "credits";
-let optionD = null;
+let optionC = "Credits";
+let optionD = "How to Play";
 
 //option selection
-let selectNumber = 1;
+let selectNumber = 0;
 let selected = false;
+let pointer = new Image();
+pointer.src = "pointer.draw";
+
+//game states
+let state = "menu";
 
 window.addEventListener('keydown', e =>{
   switch(e.key){
@@ -40,15 +45,35 @@ window.addEventListener('keydown', e =>{
   }
 });
 
-window.addEventListener('click', e =>{
-  
+window.addEventListener('click', e => {
+  console.log(e.x, e.y)
 });
 
 function animate(){
-  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  ctx.font = 50 + 'px' + 'Hevetica';
-  
+  //menu and boards
+  if(state === "menu"){
+  ctx.font = 25 + 'px ' + 'Courier New';
+  ctx.fillText(middleMessage, CANVAS_WIDTH/2-170, 150);
+  }
+ 
+  ctx.font = 18 + 'px ' + 'Courier New';
+  ctx.fillText(optionA, 106, 180);
+  ctx.fillText(optionB, 106, 210);
+  ctx.fillText(optionC, 106, 240);
+  ctx.fillText(optionD, 106, 270);
+
+if(!state === "menu"){
+  ctx.font = 12 + 'px ' + 'Courier New';
+  ctx.fillText("HEALTH: " + health, 150, 100);
+  ctx.fillText("MONEY: " + money, 250, 100);
+  ctx.fillText("ENERGY: "+ energy, 350, 100);
+  }
+
+  //arrow pointer and selection
+  ctx.fillRect(70, 165 + 30* (selectNumber % 4), 20, 20);
+    
   requestAnimationFrame(animate);
 }
 animate();
